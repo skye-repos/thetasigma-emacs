1b2fdbf8dec764526ccc299223a321297967e694
Skye <skye@Skyes-MacBook-Pro.local>
2024-08-05 23:12:35 +0530

    Updated README
    Signed-off-by: Skye <skye@Skyes-MacBook-Pro.local>

--- thetasigma-user/theme-loader.el
+++
@@ -1,3 +0,0 @@
-;; Load the Θ Σ Emacs Theme.
-(add-to-list 'custom-theme-load-path "~/.emacs.d/thetasigma-user/")
-(load-theme 'thetasigma t)
--- thetasigma-user/ui-setup.el
+++ thetasigma-user/ui-setup.el
@@ -1,2 +1,6 @@
+;; Load the Θ Σ Emacs Theme.
+(add-to-list 'custom-theme-load-path "~/.emacs.d/thetasigma-user/")
+(load-theme 'thetasigma t)
+
 ;; Not necessary but recommended. Pick the font of your choice.
 (set-face-attribute 'default nil :family "0xProto" :height 240)
