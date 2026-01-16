3b2244e180118e69540bb75cf47850632bdadb86
Skye <skye@Skyes-MacBook-Pro.local>
2026-01-16 13:47:12 +0530

    yasnippet. I give in

--- 1-main/3-completions.el
+++ 1-main/3-completions.el
@@ -26,27 +26,35 @@
 	(corfu-history-mode . t)
 	(corfu-popupinfo-mode . t)))
 
-;; Tempel
-(leaf tempel
-  :ensure t
-  :bind
-  '(("C-c i" . tempel-complete)
-	("C-<tab>" . tempel-next)
-	("S-<tab>" . tempel-previous))
-  :custom
-  '((tempel-trigger-prefix . "<"))
-  :config
-  (defun tempel-setup-capf ()
-    "Setup templ Capf endpoint."
-    (setq-local completion-at-point-functions
-		(cons #'tempel-expand
-                      completion-at-point-functions)))
-  :hook
-  '((conf-mode-hook . tempel-setup-capf)
-	(prog-mode-hook . tempel-setup-capf)
-	(text-mode-hook . tempel-setup-capf)))
+;; ;; Tempel
+;; (leaf tempel
+;;   :ensure t
+;;   :bind
+;;   '(("C-c i" . tempel-complete)
+;; 	("C-<tab>" . tempel-next)
+;; 	("S-<tab>" . tempel-previous))
+;;   :custom
+;;   '((tempel-trigger-prefix . "<"))
+;;   :config
+;;   (defun tempel-setup-capf ()
+;;     "Setup templ Capf endpoint."
+;;     (setq-local completion-at-point-functions
+;; 		(cons #'tempel-expand
+;;                       completion-at-point-functions)))
+;;   :hook
+;;   '((conf-mode-hook . tempel-setup-capf)
+;; 	(prog-mode-hook . tempel-setup-capf)
+;; 	(text-mode-hook . tempel-setup-capf)))
 
-(leaf tempel-collection
+;; (leaf tempel-collection
+;;   :ensure t)
+
+(leaf yasnippet
+  :ensure t
+  :config
+  (yas-minor-mode t))
+
+(leaf yasnippet-snippets
   :ensure t)
 
 ;; Which Key
